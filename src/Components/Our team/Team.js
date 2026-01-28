5f9576da1e23ece87a41d6912a6c2867df65332f
Muhammad Saad Asghar <muhammadsaadasghar408@gmail.com>
2026-01-28 22:38:59 +0500

    create team component

--- src/Components/Our team/Team.js
+++ src/Components/Our team/Team.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Team.css";
-import person from "../../logo/person.png"
+import person from "../../logo/person.png";
 
 const Team = () => {
   return (
@@ -11,23 +11,152 @@ const Team = () => {
             <h1>Our Team</h1>
           </div>
           <div className="Team-p">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga quos quaerat sapiente nam, id vero.</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga
+              quos quaerat sapiente nam, id vero.
+            </p>
           </div>
         </div>
 
         <div className="Team-card">
-            <div className="Team-person">
-                <div className="Team-image">
-                    <figure className="team-img">
-                        <img src={person} alt="" />
-                    </figure>
-                    <figcaption className="img-social">
-    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-    <a href="#"><i class="fa-brands fa-twitter"></i></a>
-    <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                    </figcaption>
-                </div>
+          <div className="Team-person">
+            <div className="Team-image">
+              <figure className="team-img">
+                <img src={person} alt="" />
+              </figure>
+              <figcaption className="img-social">
+                <a href="#">
+                  <i class="fa-brands fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-instagram"></i>
+                </a>
+              </figcaption>
             </div>
+            <div className="card-name">
+              <h2 className="card-h2">Rao Saad</h2>
+              <span className="card-title">presedent</span>
+              <p className="card-p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum excepturi corporis qui doloribus perspiciatis ipsa modi accusantium repellat.</p>
+            </div>
+          </div>
+          <div className="Team-person">
+            <div className="Team-image">
+              <figure className="team-img">
+                <img src={person} alt="" />
+              </figure>
+              <figcaption className="img-social">
+                <a href="#">
+                  <i class="fa-brands fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-instagram"></i>
+                </a>
+              </figcaption>
+            </div>
+            <div className="card-name">
+              <h2 className="card-h2">Rao Saad</h2>
+              <span className="card-title">presedent</span>
+              <p className="card-p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum excepturi corporis qui doloribus perspiciatis ipsa modi accusantium repellat.</p>
+            </div>
+          </div>
+          <div className="Team-person">
+            <div className="Team-image">
+              <figure className="team-img">
+                <img src={person} alt="" />
+              </figure>
+              <figcaption className="img-social">
+                <a href="#">
+                  <i class="fa-brands fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-instagram"></i>
+                </a>
+              </figcaption>
+            </div>
+            <div className="card-name">
+              <h2 className="card-h2">Rao Saad</h2>
+              <span className="card-title">presedent</span>
+              <p className="card-p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum excepturi corporis qui doloribus perspiciatis ipsa modi accusantium repellat.</p>
+            </div>
+          </div>
+          <div className="Team-person">
+            <div className="Team-image">
+              <figure className="team-img">
+                <img src={person} alt="" />
+              </figure>
+              <figcaption className="img-social">
+                <a href="#">
+                  <i class="fa-brands fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-instagram"></i>
+                </a>
+              </figcaption>
+            </div>
+            <div className="card-name">
+              <h2 className="card-h2">Rao Saad</h2>
+              <span className="card-title">presedent</span>
+              <p className="card-p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum excepturi corporis qui doloribus perspiciatis ipsa modi accusantium repellat.</p>
+            </div>
+          </div>
+          <div className="Team-person">
+            <div className="Team-image">
+              <figure className="team-img">
+                <img src={person} alt="" />
+              </figure>
+              <figcaption className="img-social">
+                <a href="#">
+                  <i class="fa-brands fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-instagram"></i>
+                </a>
+              </figcaption>
+            </div>
+            <div className="card-name">
+              <h2 className="card-h2">Rao Saad</h2>
+              <span className="card-title">presedent</span>
+              <p className="card-p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum excepturi corporis qui doloribus perspiciatis ipsa modi accusantium repellat.</p>
+            </div>
+          </div>
+          <div className="Team-person">
+            <div className="Team-image">
+              <figure className="team-img">
+                <img src={person} alt="" />
+              </figure>
+              <figcaption className="img-social">
+                <a href="#">
+                  <i class="fa-brands fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i class="fa-brands fa-instagram"></i>
+                </a>
+              </figcaption>
+            </div>
+            <div className="card-name">
+              <h2 className="card-h2">Rao Saad</h2>
+              <span className="card-title">presedent</span>
+              <p className="card-p1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum excepturi corporis qui doloribus perspiciatis ipsa modi accusantium repellat.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
